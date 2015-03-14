b13ef7cde3a3dde9a38884fd2a5f86e69bb18d50
Jakub Olejník <olejnjak@gmail.com>
2015-03-14 18:43:28 +0100

    Added library header

---
+++ Objective-Zip.h
@@ -0,0 +1,13 @@
+//
+//  Objective-Zip.h
+//  Objective-Zip
+//
+//  Created by Jakub Olejník on 14/03/15.
+//
+//
+
+#import "ZipFile.h"
+#import "ZipException.h"
+#import "ZipWriteStream.h"
+#import "FileInZipInfo.h"
+#import "ZipReadStream.h"
